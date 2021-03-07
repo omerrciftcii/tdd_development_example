c6a499ce1bb3419f3683c9c39af0ae49cb74eb95
Ömer Çiftci <omerrciftcii@hotmail.com>
2021-03-07 10:34:29 +0300

    This is First Commit

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,12 @@
+import 'package:clean_code/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({@required int number}){
+    
+  }
+}
